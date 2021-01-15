aaf4f8edd54553e6412d435cb5044180fa0e25f9
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-15 16:33:15 +0200

    /new and /logout are added

--- routes.py
+++ routes.py
@@ -38,3 +38,12 @@ def register():
             return redirect("/")
         else:
             return render_template("error.html",message="Rekisteröinti ei onnistunut")
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
