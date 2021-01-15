f50603aae030459ec0d45301d8639c14d2f81aa6
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-15 11:35:46 +0200

    /register route

--- routes.py
+++ routes.py
@@ -2,3 +2,27 @@
 def index():
     list = messages.get_list()
     return render_template("index.html", count=len(list), messages=list)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Rekisteröinti ei onnistunut")
