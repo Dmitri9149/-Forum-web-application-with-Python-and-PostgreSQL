543526c8e5388035bce431bfafaebc24c71f0599
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-15 16:21:17 +0200

    some changes

--- messages.py
+++ messages.py
@@ -3,8 +3,7 @@ import users
 
 
 def get_list():
-    sql = "SELECT M.content, U.username, M.sent_at FROM messages M, users U" \
-          "WHERE M.user_id=U.id ORDER BY M.id"
+    sql = "SELECT M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id"
     result = db.session.execute(sql)
     return result.fetchall()
 
